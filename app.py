3152d4de3b0d03eeacff0856718dd7eed50fa5b4
Cherry <fernandotb281005@gmail.com>
2023-10-25 11:43:00 -0600

    update

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask, render_template, Response
+from flask import Flask, request, render_template, redirect, url_for, Response
+from facenet_pytorch import InceptionResnetV1
 import cv2
 import numpy as np
 from mtcnn.mtcnn import MTCNN
 import torch
 import pymysql
 import mediapipe as mp
-from facenet_pytorch import InceptionResnetV1
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
-
-mp_face_mesh = mp.solutions.face_mesh
-mp_drawing = mp.solutions.drawing_utils
 
 # Cargar el modelo InceptionResnetV1
 encoder = InceptionResnetV1(pretrained='vggface2', classify=False).eval()
 
-# Crear una instancia de MTCNN
-mtcnn = MTCNN()
+mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
 
 # Conectar a la base de datos MySQL
 conn = pymysql.connect(host='localhost', user='root', password='', database='app-recognition')
@@ -33,7 +32,7 @@ def generate_frames():
         while True:
             ret, frame = cap.read()
 
-            caras = mtcnn.detect_faces(frame)
+            caras = MTCNN.detect_faces(frame)
 
             if ret and len(caras) > 0:
                 x2, y2, width2, height2 = caras[0]['box']
@@ -92,6 +91,66 @@ def reconocimiento_facial():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Ruta para el formulario
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+# Ruta para procesar el formulario
+@app.route('/procesar', methods=['POST'])
+def procesar():
+    nombre_usuario = request.form['nombre_usuario']
+    imagen = request.files['imagen']
+
+    # Procesar la imagen y almacenar el descriptor facial
+    descriptor_bytes = procesar_imagen(imagen)
+
+    if descriptor_bytes is not None:
+        # Almacenar el descriptor facial en la base de datos junto con el nombre de usuario
+        insert_query = "INSERT INTO tabla_descriptores (descriptor_data, nombre_usuario) VALUES (%s, %s)"
+        cursor.execute(insert_query, (descriptor_bytes, nombre_usuario))
+        conn.commit()  # Guardar los cambios en la base de datos
+        return "Descriptor facial almacenado en la base de datos para el usuario " + nombre_usuario
+    else:
+        return "No se detectaron caras en la imagen o hubo un error en el procesamiento."
+
+def procesar_imagen(imagen):
+    # Cargar la imagen
+    image = cv2.imdecode(np.fromstring(imagen.read(), np.uint8), cv2.IMREAD_COLOR)
+
+    # Crear una instancia de MTCNN
+    mtcnn = MTCNN()
+
+    # Detectar caras en la imagen
+    caras = mtcnn.detect_faces(image)
+
+    if len(caras) > 0:
+        # Extraer la región de la cara
+        x, y, width, height = caras[0]['box']
+        face = image[y:y+height, x:x+width]
+
+        # Redimensionar la cara a 160x160
+        face = cv2.resize(face, (160, 160))
+
+        # Preprocesar la imagen para el modelo InceptionResnetV1
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        face = np.transpose(face, (2, 0, 1))
+        face = face / 255.0
+        face_tensor = torch.tensor(face, dtype=torch.float32)
+        face_tensor = face_tensor.unsqueeze(0)
+
+        # Generar el embedding de la cara
+        embedding_cara = encoder(face_tensor)
+
+        # Convertir el embedding de la cara en un arreglo NumPy
+        embedding_array = embedding_cara.detach().numpy()
+
+        # Convertir el arreglo NumPy en bytes
+        descriptor_bytes = embedding_array.tobytes()
+        return descriptor_bytes
+    else:
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
